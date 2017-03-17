919ca438c39439cc970dad68d6fc731245117468
Ilya Istomin <iistomin@gds000591-nb.carina.griddynamics.net>
2017-03-17 22:25:59 +0400

    Initial commit

---
+++ src/main/java/com/ottepel_hack/SpaceSheepApplication.java
@@ -0,0 +1,12 @@
+package com.ottepel_hack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceSheepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceSheepApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ottepel_hack/SpaceSheepApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ottepel_hack;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpaceSheepApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
